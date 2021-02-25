0d2b0ba62110a12facaddc2a19b9661608d32dbc
Rekha0811 <71653503+Rekha0811@users.noreply.github.com>
2021-02-25 20:19:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup(){createCanvas(400,400)
+                box = createSprite(150,150,20,20);}
+
+function draw(){
+  background('black')
+  if(mousePressedOver(box)){
+    console.log("Dont click on the Box")
+     fill("white");
+    textSize(20);
+    text("Dont click on the Box",100,100);
+  }
+  if(touches.length >0){
+    console.log(touches)
+    for(var i=0;i<touches.length;i++){
+      if(touches[i][0] == box.x &&touches[i][1] == box.y){
+        console.log("true")
+         fill("white");
+    textSize(20);
+    text("Dont touch the Box",100,100);
+      }
+      else
+        {
+          console.log("false")
+        }
+        
+    }
+  }
+  drawSprites();
+}
